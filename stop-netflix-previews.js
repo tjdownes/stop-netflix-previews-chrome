5b69dfefa161324e86b55d0abd8bde791f4897ff
TJ Downes <tj.downes@fpx.com>
2018-07-17 11:29:59 -0700

    Refactored due to scope issues and some NetFlix DOM trickiness

--- stop-netflix-previews.js
+++ stop-netflix-previews.js
@@ -1,10 +1,25 @@
-DomReady.ready(function() {
+domready(function() {
   console.warn('Stop NetFlix Previews extension enabled.');
-  var videoPlayer = document.querySelector(".billboard-row .VideoContainer video");
-  videoPlayer.addEventListener("play", pausePreviewPlayer, true);
+
+  function setTimer() {
+    return window.setInterval(function() {
+      addPlayerEventListener();
+    }, 500)
+  }
+
+  var timer = setTimer();
+
+  function addPlayerEventListener() {
+    var videoPlayer = document.querySelector(".billboard-row .VideoContainer video");
+    if(videoPlayer) {
+      videoPlayer.addEventListener("play", pausePreviewPlayer, true);
+      window.clearInterval(timer);
+      timer = null;
+    }
+  }
 
   function pausePreviewPlayer() {
-      videoPlayer.pause();
+      this.pause();
       console.warn('NetFlix Previews paused.');
   }
 });
